8dace84ab0a73abf3074cac951753824be542664
Frans de Graaff <frans.degraaff@uniqodo.com>
2018-06-15 10:58:31 +0100

    Try to use babel transform plugins to support es3

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -12,6 +12,25 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(['dist']),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['es3'],
+            plugins: [
+              "transform-es2015-modules-commonjs",
+              "transform-es3-member-expression-literals", 
+              "transform-es3-property-literals"
+            ]
+          }
+        }
+      }
+    ]
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
