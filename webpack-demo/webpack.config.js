fc0866eb35d4f163ed60f1c68bf4a1be6d3b3fb6
Frans de Graaff <frans.degraaff@uniqodo.com>
2018-06-19 20:47:34 +0100

    Try to duplicate cjs style in es6

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,30 +1,11 @@
 const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: {
     app: [
-      "babel-polyfill",
       './src/index.js'
     ],
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env', 'es3']
-          }
-        }
-      }
-    ]
   },
   output: {
     filename: '[name].bundle.js',
